dfbda150361fc5ec6504598fa710047089b64e71
shahboz2001 <shakhbozbek2001@gmail.com>
2023-02-13 21:24:09 +0500

    qoshish amali qoshildi

--- asosiy/views.py
+++ asosiy/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render,redirect
 from .models import *
 # Create your views here.
 def hamma_todo(request):
+    if request.method=="POST":
+        Kundalik.objects.create(
+            vaqti=request.POST.get('v'),
+            izoh=request.POST.get('i'),
+            maqsad=request.POST.get("m")
+        )
+        return redirect("/todo/")
     data={"todo":Kundalik.objects.all()}
     return render(request,"todo.html",data)
 def kundalik_edit(request,son):
