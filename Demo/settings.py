1829e1bdad1d8a4632e2da9b7030b832d18c69a5
shahboz2001 <shakhbozbek2001@gmail.com>
2023-02-13 17:57:37 +0500

    hamma todolar chiqadi

--- Demo/settings.py
+++ Demo/settings.py
@@ -45,7 +45,8 @@ ROOT_URLCONF = 'Demo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- asosiy/views.py
+++ asosiy/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .models import *
 # Create your views here.
+def hamma_todo(request):
+    data={"todo":Kundalik.objects.all()}
+    return render(request,"todo.html",data)
